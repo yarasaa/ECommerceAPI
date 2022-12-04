358133c6c8f0143420069f5d5c94f2e753ed75c3
Mehmet Akbaba <mehmet.akbaba2011@gmail.com>
2022-12-05 00:02:02 +0300

    Repository Design Pattern

--- Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using ECommerceAPI.Persistence.Contexts;
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Persistence.Contexts;
+using ECommerceAPI.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,7 +20,15 @@ namespace ECommerceAPI.Persistence
         {
            
 
-            services.AddDbContext<ECommerceAPIDbContext>(options=>options.UseNpgsql(Configuration.ConnectionString));
+            services.AddDbContext<ECommerceAPIDbContext>(options=>options.UseNpgsql(Configuration.ConnectionString),ServiceLifetime.Singleton);
+            services.AddSingleton<ICustomerReadRepository,CustomerReadRepository>();
+            services.AddSingleton<ICustomerWriteRepository,CustomerWriteRepository>();
+            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
+            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
+            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
+          
+
         }
     }
 }
---
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using ECommerceAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository,IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+        [HttpGet]
+        public async void Get()
+        {
+            _productWriteRepository.AddRangeAsync(new() { 
+            new() { Id=Guid.NewGuid(),ProductName="Test Ürün",Price=100,CreatedDate=DateTime.UtcNow,Stock=10},
+            new() { Id=Guid.NewGuid(),ProductName="Test Ürün1",Price=200,CreatedDate=DateTime.UtcNow,Stock=20},
+            new() { Id=Guid.NewGuid(),ProductName="Test Ürün2",Price=300,CreatedDate=DateTime.UtcNow,Stock=30},
+            });
+            await _productWriteRepository.SaveAsync();
+        }
+    }
+}
